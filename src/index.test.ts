e43e0ed472822e88abd8100245d28357213fcdfb
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-08-19 06:57:30 +0100

    Simple test working, positive, no non-wd

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,11 @@
 import { spawnSync } from 'child_process';
 
 describe('Date Calculation Script', () => {
-  it('should print out the parsed JSON', () => {
-    const inputJSON = JSON.stringify({
-      key: 'value',
-    });
+  it.each([{
+    StartDate: '24-05-2004 12:05',
+    Increment: 2,
+  }])('should output the correct date', (input) => {
+    const inputJSON = JSON.stringify(input);
 
     const result = spawnSync('npx', [
       'ts-node',
@@ -13,7 +14,7 @@ describe('Date Calculation Script', () => {
       inputJSON,
     ]);
 
-    expect(result.stdout.toString().trim()).toBe('{"key":"value"}');
+    expect(result.stdout.toString().trim()).toContain('26-05-2004 12:05');
     expect(result.error).toBeFalsy();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import yargs, { Argv } from 'yargs';
+import { addDays, set, isWeekend, isBefore, isAfter, format } from 'date-fns';
+import { parse } from 'date-fns';
+import { utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz';
+
+const WORK_START_HOUR = 8;
+const WORK_END_HOUR = 16;
+const WORK_WEEKDAYS = [1, 2, 3, 4, 5]; // Monday to Friday
 
 const argv = yargs(process.argv.slice(2)).options({
   json: {
@@ -9,9 +16,51 @@ const argv = yargs(process.argv.slice(2)).options({
 }).argv as { json: string };
 
 try {
-  const jsonObject = JSON.parse(argv.json);
-  const outputString = JSON.stringify(jsonObject);
-  console.log(outputString);
+  const input = JSON.parse(argv.json);
+
+  if (!input.StartDate || !input.Increment) {
+    throw new Error('StartDate and Increment are required.');
+  }
+
+  const dateFormat = 'dd-MM-yyyy HH:mm'; // Define the format of the input date string
+
+  const startDate = parse(input.StartDate, dateFormat, new Date(), {
+    useAdditionalWeekYearTokens: true,
+  });
+  // console.log(`id: ${inputDate}`);
+  // const startDate = zonedTimeToUtc( inputDate, 'Europe/London');
+
+  console.log(`sd: ${startDate}`);
+  const increment = parseFloat(input.Increment);
+
+  let currentDate = startDate;
+  let daysToAdd = Math.abs(increment);
+  console.log(daysToAdd);
+  while (daysToAdd > 0) {
+    currentDate = addDays(currentDate, increment < 0 ? -1 : 1);
+    console.log(currentDate);
+
+    // Skip weekends
+    if (isWeekend(currentDate)) {
+      continue;
+    }
+
+    const workStart = set(currentDate, { hours: WORK_START_HOUR, minutes: 0 });
+    const workEnd = set(currentDate, { hours: WORK_END_HOUR, minutes: 0 });
+
+    console.log(`ws: ${workStart}`);
+    if (isBefore(currentDate, workStart)) {
+      currentDate = workStart;
+    } else if (isAfter(currentDate, workEnd)) {
+      currentDate = addDays(workStart, 1);
+    }
+
+    daysToAdd--;
+  }
+
+  const formattedResult = format(currentDate, 'dd-MM-yyyy HH:mm');
+
+  console.log(formattedResult);
 } catch (error: any) {
   console.error('Error processing JSON:', error.message);
 }
